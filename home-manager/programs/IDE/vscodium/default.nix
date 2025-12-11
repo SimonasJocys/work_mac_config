d3cc89b679f5b1d79ce4f1d6f5a3591eb2505155
SimonasJocys <simonasjocys@gmail.com>
2025-12-11 12:45:29 +0200

    vscodium small update

--- home-manager/programs/IDE/vscodium/default.nix
+++ home-manager/programs/IDE/vscodium/default.nix
@@ -2,10 +2,10 @@
 
 {
   # move to separate file?
-  home.packages = [ 
+  home.packages = [
     pkgs.nil
     pkgs.vscodium
-     ];
+  ];
 
   programs.vscode = {
     enable = true;
@@ -19,11 +19,13 @@
         "editor.fontSize" = 14;
         "terminal.integrated.fontFamily" = "'JetBrains Mono'";
         "terminal.integrated.fontSize" = 14;
-        "update.mode" = "none"; #fixing when tries to update and fails. annoying popup everytime
-        # "remote.SSH.syncConfiguration" = false;
-        # "remote.downloadExtensionsLocally" = true;
+        "update.mode" = "none"; # fixing when tries to update and fails. annoying popup everytime
+        
+        "remote.SSH.syncConfiguration" = false; # was commented before
+        "remote.downloadExtensionsLocally" = true; # was commented before
+
         "workbench.colorTheme" = "One Dark Pro";
-        "remote.autoForwardPortsSource"= "hybrid"; #a bit shady, no?
+        "remote.autoForwardPortsSource" = "hybrid"; # a bit shady, no?
       };
       keybindings = [
         {
@@ -31,46 +33,51 @@
           command = "editor.multiCursorModifier";
         }
       ];
-      
-    extensions = with pkgs.vscode-extensions; 
-  ([
+
+      extensions =
+        with pkgs.vscode-extensions;
+        [
   # https://github.com/nix-community/vscode-nix-ide - check additional configurations for nix-ide
   # ghcopilot seems to be needing alot of manual setup due to ms restrictions
   # no idea why, but capital letters here break the confing...
   # github.copilot
   # github.copilot-chat
-    jnoortheen.nix-ide
-    mechatroner.rainbow-csv
-    zhuangtongfa.material-theme
-
-    ms-python.python
-    ms-toolsai.jupyter
-    ms-python.black-formatter
-  ] 
-  # this seems to be the way to install open-vsx.org extentions
-  ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    {
-      name = "nextflow";
-      publisher = "nextflow";
-      version = "1.5.2";
-      sha256 = "VxpxBmZ7y0cEX4YOWvXXf3l2se0LThYysEH1yplB67I="; # use nix-prefetch or leave placeholder
-    }
-    {
-      name = "snakemake-lang";
-      publisher = "snakemake";
-      version = "0.7.0";
-      sha256 = "KKUYoEyYEsXNkRpqsCQOM/1pirO2DT6wTwkGsFbFxJ0="; # use nix-prefetch or leave placeholder
-    }
-  ]);
 
 
+  # also: snakemake and github extentions seems to be "breaking everyting related to extentions, that sometimes they are the only ones installed"          jnoortheen.nix-ide
+          mechatroner.rainbow-csv
+          zhuangtongfa.material-theme
+          ms-python.python
+          ms-toolsai.jupyter
+          ms-python.black-formatter
+          jnoortheen.nix-ide
 
+        ]
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+# this seems to be the way to install open-vsx.org extentions
 
-
+          {
+            name = "nextflow";
+            publisher = "nextflow";
+            version = "1.5.2";
+            sha256 = "VxpxBmZ7y0cEX4YOWvXXf3l2se0LThYysEH1yplB67I=";
+          }
+          {
+            name = "snakemake-lang";
+            publisher = "snakemake";
+            version = "0.7.0";
+            sha256 = "KKUYoEyYEsXNkRpqsCQOM/1pirO2DT6wTwkGsFbFxJ0=";
+          }
+          # does not seem to work. Need manual install. Maybe add as separate install with let statement?
+          # {
+          #   name = "open-remote-ssh";
+          #   publisher = "jeanp413";
+          #   version = "0.0.48";
+          #   sha256 = "QfJnAAx+kO2iJ1EzWoO5HLogJKg3RiC3hg1/u2Jm6t4="; # use nix-prefetch or leave placeholder
+          # }
+        ];
 
     };
-
-    
 
   };
 
