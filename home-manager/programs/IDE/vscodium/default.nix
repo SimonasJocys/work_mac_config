37ddddff24e68f06770088e3eb936600516b713f
SimonasJocys <simonasjocys@gmail.com>
2025-12-11 11:33:43 +0200

    removing ghcopilot extention - to much hassle in vscodium

--- home-manager/programs/IDE/vscodium/default.nix
+++ home-manager/programs/IDE/vscodium/default.nix
@@ -31,18 +31,27 @@
           command = "editor.multiCursorModifier";
         }
       ];
+      # no idea why, but capital letters here break the confing...
       extensions = with pkgs.vscode-extensions; [
         # https://github.com/nix-community/vscode-nix-ide - check additional configurations for nix-ide
+        # ghcopilot seems to be needing alot of manual setup due to ms restrictions
+        # github.copilot
+        # github.copilot-chat
         jnoortheen.nix-ide
         mechatroner.rainbow-csv
-        GitHub.copilot
+        zhuangtongfa.material-theme
+
+        # Open-VSX extension needs to be downloaded manually. Check if this persists after hms 
+        # open_remote_openvsx
+
         # nextflow.nextflow
         # snakemake.snakemake-lang
         # yukina.yukinord
         # asvetliakov.vscode-neovim
-
       ];
     };
+
+    
 
   };
 
